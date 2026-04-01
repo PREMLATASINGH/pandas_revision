573cc36fbd1ea3135e6a1f27334ae3c2655fc22b
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-04-01 18:21:47 -0400

    unique total_sales

--- pro.py
+++ pro.py
@@ -33,6 +33,6 @@ df4=df.groupby(['region','category'])['total_sales'].sum()
 print(df4)
 df5=df['total_sales'].sort_values(ascending=True)
 print(df5)
-df6=df['category'].unique()
+df6=df['total_sales'].unique()
 print(df6)
 
