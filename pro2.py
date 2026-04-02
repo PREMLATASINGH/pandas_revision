a0dd7fff0d8cdb7659e80788820e74aaa544dd95
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-04-02 16:15:54 -0400

    added new pro

---
+++ pro2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+data={
+     "product":["laptop","phone","chair","table","laptop",'airpot','table','chair'],
+    "category":["tech","tech","furniture","furniture","tech",'tech',"furniture","furniture"],
+    "quantity":[1,2,3,1,2,7,4,5],
+    "price":[1200,800,150,300,1200,500,675,150],
+    "sales":[900,500,100,200,900,700,800,890],
+    "region":["east","west","east","south","west","east","west","east"],
+    "date":['2024-01-01','2024-01-02','2024-01-03','2024-01-04','2024-01-01','2024-01-05','2024-01-06','2024-01-07']
+}
+df=pd.DataFrame(data)
+print(df)
