3ea2b4fa1428e10a22a0b3e4f76ca79b2c453f0d
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-04-04 09:50:48 -0400

    print table

---
+++ pro5.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd
+Data={'emp_name':["A","B","C","D","E","F","G","H","I","J"],
+      "department":['HR','IT','Finance','HR','IT','Finance','HR','IT','Finance','HR'],
+      "salary":[50000,60000,55000,52000,62000,58000,51000,61000,57000,53000],
+      "experience":[2,5,3,4,6,1,3,7,2,4]
+
+}
+df=pd.DataFrame(Data)
+print(df)
