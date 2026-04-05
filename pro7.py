abeb1566d8c888bf21622eb8eaed21d6387eea89
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-04-05 10:32:20 -0400

    new data

---
+++ pro7.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data={
+    'region':['North','South','East','West','North','South','East','West'],
+    'sales':[100,150,200,250,300,350,400,450],
+    'product':['A','B','C','D','A','B','C','D']
+}
+df=pd.DataFrame(data)
+print(df)
