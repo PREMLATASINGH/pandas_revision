959ba37a78c4b07a0ef94f1d8c754e13bcd3fa12
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-04-06 05:17:24 -0400

    print table

---
+++ pro8.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+data={
+    'order_id':[1,2,3,4,5,6,7,8],
+    'region':['North','South','East','West','North','South','East','West'],
+    'sales':[100,150,200,250,300,350,400,450],
+    'product':['A','B','C','D','A','B','C','D'],
+    'profit':[10,15,20,25,30,35,40,45]
+}
+df=pd.DataFrame(data)
+print(df)
