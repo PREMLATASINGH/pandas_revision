153f285b1add2b4ccd53c8494cc8189577cda172
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-06-02 14:55:43 -0400

    added

---
+++ pro9.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+data={
+    'product_id':[101,102,103,104,105],
+    'product_name':['Laptop','Smartphone','Tablet','Headphones','Smartwatch'],  
+    'category':['Electronics','Electronics','Electronics','Accessories','Electronics'],
+    'price':[1000,800,600,150,200],
+    'stock':[50,100,80,200,150],
+    'release_date':['2021-01-10','2020-05-15','2021-08-20','2019-11-05','2020-09-30']
+}
+df=pd.DataFrame(data)
+print(df)
+df.loc[2,'price']=np.nan
+print(df)
+print(df.dropna())
+print(df.fillna(df['price'].mean()))
+print(df['price'].isnull().sum())
+
