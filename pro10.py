2d6419dcc7f995b36fae0562830c77663e71173a
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-06-03 14:22:38 -0400

    first five row

--- pro10.py
+++ pro10.py
@@ -7,7 +7,7 @@ data={
     'years_experience':[1,2,3,4,5,6,7,8,9,10],
     'salary':[30000,35000,40000,45000,50000,55000,60000,65000,70000,75000],
     'department':['HR','Finance','IT','Marketing','HR','Finance','IT','Marketing','HR','Finance'] 
-
 }
 df=pd.DataFrame(data)
 print(df)
+print(df.head())
