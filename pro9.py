c9704a78250517afecb9ad97c3ac8cfa449a81ae
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-08-05 15:57:11 -0400

    show columns

--- pro9.py
+++ pro9.py
@@ -25,4 +25,5 @@ print(df.sort_index(ascending=False))
 print(df.sort_values(by='price', ascending=True))
 print(df.sort_values(by=['category','price'], ascending=[True,False]))
 print(df[df['price']>500])
+print(df.columns)
 
